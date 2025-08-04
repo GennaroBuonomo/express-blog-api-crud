bdad73c06d36ea99000938ad6b63db5f6b9a1001
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-08-04 15:03:09 +0200

    Feat inserisco i vari metodi

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,2 +1,32 @@
 //importo l' array dei posts
 const posts = require('../data/posts.js');
+
+//INDEX
+const index = (req, res) => {
+
+}
+
+//SHOW
+const show = (req, res) => {
+
+}
+
+//STORE
+const store = (req, res) => {
+
+}
+
+//UPDATE 
+const update = (req, res) => {
+
+}
+
+//modify
+const modify = (req, res) => {
+
+}
+
+//DESTROY
+const destroy = (req, res) => {
+
+}
